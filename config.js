7f894e4cd12ff054023a38871e713781ca422123
Jordy Langen <jordylangen@gmail.com>
2015-10-30 20:53:52 +0100

    Added some jquery in there

--- config.js
+++ config.js
@@ -17,6 +17,7 @@ System.config({
     "babel": "npm:babel-core@5.8.33",
     "babel-runtime": "npm:babel-runtime@5.8.29",
     "core-js": "npm:core-js@1.2.3",
+    "jquery": "github:components/jquery@2.1.4",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
